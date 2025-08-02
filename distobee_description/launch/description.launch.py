5987aca429e3a3776164ec8a2ad9c002a77e666d
RafalS3 <rafalsawczyszyn@gmail.com>
2025-08-02 20:20:38 +0200

    robot structure and model test

---
+++ distobee_description/launch/description.launch.py
@@ -0,0 +1,62 @@
+from ament_index_python import get_package_share_path
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+import xacro
+
+
+urdf_path = get_package_share_path(
+    'distobee_description').joinpath('urdf', 'description.urdf.xacro')
+urdf = xacro.process(urdf_path)
+
+
+def launch_setup(context):
+    description = []
+
+    description += [
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            parameters=[{'robot_description': urdf},
+                        {"ignore_timestamp": True}],
+        ),
+    ]
+    # Only to visualize the robot model in RViz.
+    description += [
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            parameters=[{'source_list': ['/robot_description']}],
+        )
+    ]
+    # TO DO!
+    # description += [
+    #     Node(
+    #         package="joint_state_publisher",
+    #         executable="joint_state_publisher",
+    #         name="joint_state_publisher",
+    #         parameters=[
+    #             {"source_list": [...]},
+    #         ]
+    #     ),
+    # ]
+    description += [
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+            arguments=[
+                "-d", str(get_package_share_path('distobee_description') / "rviz" / "distobee.rviz")],
+        ),
+    ]
+
+    return description
+
+
+def generate_launch_description():
+
+    return LaunchDescription([
+        OpaqueFunction(function=launch_setup),
+    ])

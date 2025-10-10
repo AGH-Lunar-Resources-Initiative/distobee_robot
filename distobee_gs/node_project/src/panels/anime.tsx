e13b71cf031b11e6e355e7bb52b0f51511476ba7
Michał Proć <michalproc4@gmail.com>
2025-10-10 22:22:40 +0200

    Update anime panel, clear sieve to implement new inputs

--- distobee_gs/node_project/src/panels/anime.tsx
+++ distobee_gs/node_project/src/panels/anime.tsx
@@ -1,15 +1,24 @@
 import styles from './anime.module.css';
 
-import distobeeAnime from '!!url-loader!../media/distobee-anime.jpg';
+import animeBerry from '!!url-loader!../media/anime-berry.png';
+import animeDark from '!!url-loader!../media/anime-dark.png';
+import animeLight from '!!url-loader!../media/anime-light.png';
+import animePink from '!!url-loader!../media/anime-pink.png';
+
+const themeImages = {
+  dark: animeDark,
+  light: animeLight,
+  berry: animeBerry,
+  pink: animePink
+};
 
 export default function Anime() {
+  const theme = JSON.parse(localStorage.getItem('theme')) || 'dark';
+  const imageSrc = themeImages[theme] || animeDark;
+
   return (
     <div className={styles['anime-panel']}>
-      <img
-        src={distobeeAnime}
-        draggable='false'
-        alt=''
-      />
+      <img src={imageSrc} draggable='false' alt='' />
     </div>
   );
 }
